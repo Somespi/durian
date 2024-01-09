fe9fef0c96a320e3def84f7b5309b0c81e183171
Someone. <120342697+Somespi@users.noreply.github.com>
2024-01-10 03:50:02 +0400

    init project

--- src/main.zig
+++ src/main.zig
@@ -6,32 +6,21 @@ const rl = @cImport({
 
 
 pub fn main() anyerror!void {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+
     const screenWidth = 800;
     const screenHeight = 450;
 
-    rl.InitWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
-    defer rl.CloseWindow(); // Close window and OpenGL context
+    rl.InitWindow(screenWidth, screenHeight, "redefine");
+    defer rl.CloseWindow(); 
 
-    rl.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    rl.SetTargetFPS(60);
+    
 
-    // Main game loop
-    while (!rl.WindowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+    while (!rl.WindowShouldClose()) { 
+        
         rl.BeginDrawing();
         defer rl.EndDrawing();
 
-        rl.ClearBackground(rl.RAYWHITE);
-
-        rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LIGHTGRAY);
-        //----------------------------------------------------------------------------------
+        
     }
 }
